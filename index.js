750c937956581d1394867478c393443bceaf723d
Huy Nguyen <huyndx@outlook.com>
2020-07-01 00:44:42 +0700

    Init

---
+++ index.js
@@ -0,0 +1,11 @@
+params_parse = function(){
+    var args = process.argv;
+    var result = {};
+    for(var i = 0; i < args.length-1;i++){
+        if(args[i].indexOf("-") === 0){
+            result[args[i].substr(1)] = args[i+1];
+        }
+    }
+    return result;
+};
+module.exports = params_parse;
---
+++ readme.md
@@ -0,0 +1,4 @@
+### Params Parse
+npm install --save @ng.huyy/cc_params_parse
+
+Parse commandline args to JSON Object
